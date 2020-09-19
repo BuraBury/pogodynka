661ec7f5d207854dcc7dea92ffbf18a3f77c7f2f
Paulina <bura.bury@gmail.com>
2020-09-19 16:05:38 +0200

    up

--- src/main/java/com/burabury/weather/forecastSources/openweather/OpenWeather.java
+++ src/main/java/com/burabury/weather/forecastSources/openweather/OpenWeather.java
@@ -5,31 +5,29 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.http.client.fluent.Request;
 
 import java.io.IOException;
-import java.net.URI;
 import java.time.LocalDate;
 
 public class OpenWeather {
 
     private final static String URI_PATTERN = "https://api.openweathermap.org/data/2.5/" +
             "onecall?lat=%f&lon=%f&exclude=minutely,hourly&units=metric&appid=%s";
-    private final static ObjectMapper mapper = new ObjectMapper();
-
     private final String key;
+
+    private final static ObjectMapper mapper = new ObjectMapper();
 
     public OpenWeather(String key) {
         this.key = key;
     }
 
     public String getCurrentForecast(double lat, double lon) {
-
-        LocalDate.now();
+       LocalDate date =  LocalDate.now();
         try {
             String uri = String.format(URI_PATTERN, lat, lon, key);
             String result = Request.Get(uri)
                     .execute()
                     .returnContent()
                     .asString();
-            return String.valueOf(mapper.readValue(result, OpenWeatherResponse.class).getCurrent());
+            return date.toString() + mapper.readValue(result, OpenWeatherResponse.class).getCurrent();
         } catch (IOException e) {
             return e.getMessage();
         }
